d52d3c8585c4e4f9e5d3f893428dbb1bc765a1d4
GuillaumeM <gu.martin@yahoo.com>
2021-10-06 11:52:39 +0200

    Added modify function

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import { render } from "react-dom"
 import {
 	Container, Grid,
@@ -6,30 +6,34 @@ import {
 	ButtonGroup,
 	TextField,
 } from '@material-ui/core';
-import Person from "./Person.ts";
 
 import './style.scss';
 
-// const person = new Person();
-
 const App = (props) => {
 		
-	console.log("App render");
-
+	const textFieldTodo = useRef(null);
+	
 	const [inputValue, setInputValue] = useState();
 	const [todoList, setTodoList] = useState([]);
 
-	// useEffect(() => {
-	// 	setMessage("Hello ")
-	// 	setMessage(messList[Math.floor(Math.random()*messList.length)])
-	// }, [])
-
+	const [isEditing, setIsEditing] = useState(false);
+	const [idToEdit, setIdToEdit] = useState();
+	
 	const handleAdd = (value) => {
-		console.log(value)
 		if (value !== "") {
 			const newItem = { id: new Date().getTime().toString(), title: value };
-			setTodoList([...todoList, newItem])
+			setTodoList([...todoList, newItem]);
+			textFieldTodo.current.value = '';
+			textFieldTodo.current.focus();
 		}
+	}
+
+	const handleEdit = (value) => {
+		setIsEditing(false);
+		const toDoToEdit = todoList.find(x => x.id === idToEdit);
+		toDoToEdit.title = value;
+		textFieldTodo.current.value = '';
+		textFieldTodo.current.focus();
 	}
 
 	return (
@@ -38,15 +42,48 @@ const App = (props) => {
 			<Grid container spacing={2} justifyContent="center">
 
 				<Grid item xs={12}>
-					<TextField style={ { width:'80%', height:50, } }
-						id="outlined-basic" label="ToDo" variant="outlined"
-						onChange={(e) => { setInputValue(e.target.value) }} />
 
-					<Button variant="contained" style={ {width:'20%', height:'100%',} }
-						onClick={(e) => { handleAdd(inputValue); }}
-					>
-						Add ToDo
-					</Button>
+					{ isEditing ?
+					
+					<>
+						<TextField id="textField" style={ { width:'80%', height:50, } }
+							id="outlined-basic" label= {idToEdit + " - Edit ToDo"} variant="outlined"
+							inputRef={ textFieldTodo }
+							onKeyPress={(e) => {
+								console.log(`Pressed keyCode ${e.key}`);
+								if (e.key === 'Enter') { 
+									handleEdit(inputValue);
+									e.currentTarget.value = '';
+								}
+							}}
+							onChange={(e) => { setInputValue(e.currentTarget.value) }} />
+
+						<Button variant="contained" style={ {width:'20%', height:'100%',} }
+							onClick={(e) => { handleEdit(inputValue); }}
+						>
+							Edit ToDo
+						</Button>
+					</>
+
+					:
+
+					<>
+						<TextField style={ { width:'80%', height:50, } }
+						id="outlined-basic" label="ToDo" variant="outlined"
+						inputRef={ textFieldTodo }
+						onKeyPress={(e) => {
+							console.log(`Pressed keyCode ${e.key}`);
+							if (e.key === 'Enter') { handleAdd(inputValue); }
+						}}
+						onChange={(e) => { setInputValue(e.currentTarget.value) }} />
+
+						<Button variant="contained" style={ {width:'20%', height:'100%',} }
+							onClick={(e) => { handleAdd(inputValue); }}
+						>
+							Add ToDo
+						</Button>
+					</>
+					}
 				</Grid>
 
 				<Grid item xs={12}>
@@ -57,19 +94,26 @@ const App = (props) => {
 							<Grid item xs={4}>
 								<div>{ item.title }</div>
 								<div>{ item.id }</div>
-								<ButtonGroup variant="contained" aria-label="outlined primary button group">
+								<ButtonGroup id={item.id} variant="contained" aria-label="outlined primary button group">
 
 									<Button onClick={(e) => {
-										
-									}}>Modify</Button>
+										setIsEditing(true);
+										setIdToEdit(e.currentTarget.parentNode.id);
+										textFieldTodo.current.focus();
+									}}>
+										Modify
+									</Button>
 
 									<Button onClick={(e) => {
 										setTodoList(todoList.filter((it) => it.id !== item.id));
-									}}>Delete</Button>
+									}}>
+										Delete
+									</Button>
 
 								</ButtonGroup>
 							</Grid>
 						)) }
+						
 					</Grid>
 					
 				</Grid>
